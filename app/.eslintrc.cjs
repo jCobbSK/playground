2326aa384e0d04447e9cbdbf3c0634aa1566f7d9
Jakub Kanitra <kubo.kanitra@gmail.com>
2022-09-24 16:49:21 +0200

    Eslint explanations

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
   },
   rules: {
     'import/no-extraneous-dependencies': 0,
+
     // not sure if this is a "bug" or "feature" in airBnB config? https://github.com/airbnb/javascript/issues/2505
     'react/function-component-definition': [
       'error',
@@ -15,10 +16,18 @@ module.exports = {
         namedComponents: ['function-declaration', 'arrow-function'],
       },
     ],
+
+    // https://www.lloydatkinson.net/posts/2022/default-exports-in-javascript-modules-are-terrible/
     'import/prefer-default-export': 0,
     'import/no-default-export': 2,
+
+    // should be safe with TS
     'react/jsx-props-no-spreading': 0,
+
+    // any === Evil
     '@typescript-eslint/no-explicit-any': 2,
+
+    // TS rulez!
     'react/require-default-props': 0,
   },
   ignorePatterns: ['*.cjs'],
